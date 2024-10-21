e030a0486953deed378feab6b7dc0f123f4c64dd
zhaolihang-com <625335512@qq.com>
2024-10-21 12:16:00 +0800

    globalStorageUri

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -7,4 +7,6 @@ export async function activate(context: vscode.ExtensionContext) {
 	await detectDefaultBinaryAtStart()
 	activateWebview(context)
 	registerCommand(context)
+
+	console.log(`globalStorageUri : ${context.globalStorageUri.fsPath}`)
 }
