120d8b6ce1f875a65401484ac8068c97c07482d1
zhaolihang-com <625335512@qq.com>
2024-12-10 21:05:13 +0800

    暂时是实验性质的

--- src/extension/preview.ts
+++ src/extension/preview.ts
@@ -4,7 +4,7 @@
 // and call the vscode.diff command to display the replacement.
 // See relevant comments for TextDocumentContentProvider
 // custom scheme comes from https://stackoverflow.com/a/69384899/2198656
-import {
+import vscode, {
   Position,
   Range,
   Uri,
@@ -44,4 +44,42 @@ export function openFile({ filePath, locationsToSelect }: ChildToParent['openFil
     selection: range,
     preserveFocus: true,
   })
+
+  // const highlightDecorationType = vscode.window.createTextEditorDecorationType({
+  //   backgroundColor: 'rgba(255, 255, 0, 0.3)',
+  //   borderRadius: '3px',
+  //   border: '1px solid yellow',
+  //   color: 'red', // 文字颜色
+  //   fontWeight: 'bold',
+  //   textDecoration: 'underline'
+  // });
+
+  // // 常规搜索匹配的装饰器
+  // let findDecorations = vscode.window.createTextEditorDecorationType({
+  //   backgroundColor: new vscode.ThemeColor('editor.findMatchHighlightBackground'),
+  //   borderColor: new vscode.ThemeColor('editor.findMatchHighlightBorder'),
+  //   // 滚动条槽（overview ruler）中的标记
+  //   overviewRulerColor: new vscode.ThemeColor('editorOverviewRuler.findMatchForeground'),
+  //   overviewRulerLane: vscode.OverviewRulerLane.Center,
+  //   rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed
+  // });
+
+  // // 当前活动匹配的装饰器
+  // let activeMatchDecoration = vscode.window.createTextEditorDecorationType({
+  //   backgroundColor: new vscode.ThemeColor('editor.findMatchBackground'),
+  //   borderColor: new vscode.ThemeColor('editor.findMatchBorder'),
+  //   // 滚动条槽中的活动匹配标记
+  //   overviewRulerColor: new vscode.ThemeColor('editorOverviewRuler.findMatchForeground'),
+  //   overviewRulerLane: vscode.OverviewRulerLane.Center,
+  //   rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed
+  // });
+
+  // const decorations = [{
+  //   range: range
+  // }];
+  // const editor = vscode.window.activeTextEditor;
+  // if (editor) {
+  //   editor.setDecorations(activeMatchDecoration, decorations);
+  // }
+
 }
