553a52412f14b8ab2092f63f21ebb54ab4bd196e
zhaolihang-com <625335512@qq.com>
2025-01-09 21:14:59 +0800

    add searchx.searchInFile

--- src/extension/registerCommand.ts
+++ src/extension/registerCommand.ts
@@ -10,6 +10,7 @@ export function registerCommand(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		commands.registerCommand('searchx.searchInFolder', findInFolder),
+		commands.registerCommand('searchx.searchInFile', findInFile),
 		commands.registerCommand('searchx.refreshSearch', refreshSearch),
 		commands.registerCommand('searchx.clearSearchResults', clearSearchResults),
 		commands.registerCommand('searchx.expandAll', toggleAllSearch),
@@ -17,6 +18,19 @@ export function registerCommand(context: vscode.ExtensionContext) {
 	)
 }
 function findInFolder(data: any) {
+	const workspacePath = workspace.workspaceFolders?.[0]?.uri?.fsPath
+	// compute relative path to the workspace folder
+	const relative = workspacePath && path.relative(workspacePath, data.fsPath)
+	if (!relative) {
+		window.showErrorMessage('searchx Error: folder is not in the workspace')
+		return
+	}
+	commands.executeCommand('searchx.search.input.focus')
+	parentPort.postMessage(MessageType.S2C_SetIncludeFile, {
+		includeFile: relative,
+	})
+}
+function findInFile(data: any) {
 	const workspacePath = workspace.workspaceFolders?.[0]?.uri?.fsPath
 	// compute relative path to the workspace folder
 	const relative = workspacePath && path.relative(workspacePath, data.fsPath)
